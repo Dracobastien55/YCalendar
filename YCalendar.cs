609b50758a046fbb508efddfce77cda7978e326b
Bastien <dracobastien55@gmail.com>
2023-10-10 11:22:56 +0200

    Ajoutez des fichiers projet.

---
+++ YCalendar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace YCalendar
+{
+    /// <summary>
+    /// Classe répresentant le traitement du calendrier
+    /// </summary>
+    public class YCalendar
+    {
+
+    }
+}
